2e2b410f51da3611f02d995315e8f8950c77653a
Léo T <leo.thomas2003@orange.fr>
2025-02-03 09:18:16 +0100

    Ajout template fichiers ContenuCours

---
+++ projet_collective/lib/ui/Contenu/ContenuCoursViewModel.dart
@@ -0,0 +1,5 @@
+class CoursViewModel {
+  CoursViewModel();
+
+  
+}
---
+++ projet_collective/lib/ui/CoursSelectionne.dart
@@ -0,0 +1,9 @@
+import 'package:seriouse_game/models/cours.dart';
+
+class CoursSelectionne {
+  CoursSelectionne({
+    required Cours this.cours 
+  });
+
+  final Cours cours;
+}
