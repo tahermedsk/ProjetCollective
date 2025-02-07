4d511168cce01943969865e1af836014283f34bd
gauthier Hamel--monchot <gauthier.hamel@gmail.com>
2025-02-07 22:50:54 +0100

    semanticLabel

--- projet_collective/lib/ui/Contenu/WidgetContenu/ContenuImageWidget.dart
+++ projet_collective/lib/ui/Contenu/WidgetContenu/ContenuImageWidget.dart
@@ -4,22 +4,35 @@ import 'package:seriouse_game/ui/Contenu/ContenuCoursViewModel.dart';
 
 class ContenuImageWidget extends StatelessWidget {
   final MediaCours media;
-  final ContenuCoursViewModel viewModel = ContenuCoursViewModel(); // Instance du ViewModel
+  final double width; 
+  final double height; 
+  final ContenuCoursViewModel viewModel = ContenuCoursViewModel(); 
 
-  ContenuImageWidget({super.key, required this.media});
+  ContenuImageWidget({
+    super.key,
+    required this.media,
+    this.width = 200,  // Valeur par défaut
+    this.height = 200, // Valeur par défaut
+  });
 
   @override
   Widget build(BuildContext context) {
     String? imagePath = viewModel.imageLoader(media);
 
-    return imagePath != null
-        ? Image.asset(
-            imagePath,
-            fit: BoxFit.cover,
-            errorBuilder: (context, error, stackTrace) {
-              return const Icon(Icons.broken_image, size: 50, color: Colors.grey);
-            },
-          )
-        : const Icon(Icons.image_not_supported, size: 50, color: Colors.grey);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0), // Padding léger
+      child: imagePath != null
+          ? Image.asset(
+              imagePath,
+              width: width,  // Largeur de l'image
+              height: height, // Hauteur de l'image
+              fit: BoxFit.cover,
+              semanticLabel: media.caption, // Accessibilité
+              errorBuilder: (context, error, stackTrace) {
+                return const Icon(Icons.broken_image, size: 50, color: Colors.grey);
+              },
+            )
+          : const Icon(Icons.image_not_supported, size: 50, color: Colors.grey),
+    );
   }
 }
