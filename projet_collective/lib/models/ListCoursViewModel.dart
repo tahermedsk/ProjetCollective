a59002cd694803a2784cf23d93a54812e69dcb93
Pingrammer <108672186+Pingrammer@users.noreply.github.com>
2025-03-12 11:53:48 +0100

    ListCoursModel + Singleton

---
+++ projet_collective/lib/models/ListCoursViewModel.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:seriouse_game/repositories/coursRepository.dart';
+import 'package:seriouse_game/ui/ModuleSelectionne.dart';
+import 'cours.dart';
+
+//Classe permettant d'extraire les cours d'un module 
+class ListCoursViewModel with ChangeNotifier {
+
+  //Méthode pour changer la liste coursDuModule du Singleton ModuleSelectionne par celle correspondant à la liste des cours du module d'id idModule
+  Future<void> recupererCours(int? idModule) async {
+
+    CoursRepository repository = CoursRepository();
+
+    //On accède à la liste des cours de la base de donnée par la méthode de CoursRepository
+    ModuleSelectionne().coursDuModule = await repository.getCoursesByModuleId(idModule!);
+
+    //la liste change donc  on avertit les listeners
+    notifyListeners();
+  }
+  
+
+}
---
+++ projet_collective/lib/ui/ModuleSelectionne.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:seriouse_game/models/cours.dart';
+import 'package:seriouse_game/models/module.dart';
+
+//Singleton représentant le module sélectionné par l'utilisateur et les données qu'il contient.
+class ModuleSelectionne with ChangeNotifier {
+
+  late Module moduleSelectionne;
+
+  static final ModuleSelectionne instance = ModuleSelectionne._internal() ;
+
+  //Liste contenant les cours d'un module
+  List<Cours> coursDuModule = List.empty() ;
+  
+  static ModuleSelectionne getInstance(){
+
+    return instance;
+
+  }
+
+  factory ModuleSelectionne() {
+    return instance;
+  }
+
+
+  ModuleSelectionne._internal();
+
+  //Permet de modifier le module selectionné tout en avertissant les listeners
+  void changeModule(Module module){
+
+    moduleSelectionne = module;
+    notifyListeners();
+
+  }
+
+  //Permet de mettre à jour la liste des cours du module
+  void updateListModule(List<Cours> listCours){
+    coursDuModule = listCours;
+    notifyListeners();
+  }
+
+}
