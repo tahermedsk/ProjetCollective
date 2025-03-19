a9e4d9460f6d74bcfefc94c73f888a96bde15302
Yassine <yassine.ellatifi096@gmai.com>
2025-03-19 12:06:48 +0100

    changement style du header et footer globale + ajout d'asset + ajout du launch screen avec animation au lancement de l'application.

--- projet_collective/lib/ui/App.dart
+++ projet_collective/lib/ui/App.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:seriouse_game/ui/LaunchScreen/LaunchScreenView.dart';
 import 'package:seriouse_game/ui/ListCoursView.dart';
 import 'package:seriouse_game/ui/ListModuleView.dart';
 import 'package:seriouse_game/ui/Cours/CoursView.dart';
-import 'package:go_router/go_router.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
 final _shellNavigatorKey = GlobalKey<NavigatorState>();
@@ -28,23 +29,22 @@ class App extends StatefulWidget {
   final Widget? child;
 
   @override
-  State<App> createState() =>
-      _AppState();
+  State<App> createState() => _AppState();
 }
 
 class _AppState extends State<App> {
   int currentIndex = 0;
+  bool showLaunchScreen = true;
 
   void changeTab(int index) {
     switch (index) {
       case 0:
-        context.go('/cours'); // context.go('/');
+        context.go('/cours');
         break;
       case 1:
         context.go('/module');
         break;
       case 2:
-        // La certification n'est pas implémentée
       default:
         context.go('/');
         break;
@@ -55,127 +55,78 @@ class _AppState extends State<App> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(252, 179, 48, 1),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'lib/data/AppData/CharteFactoscope/logo-factoscope_seul.png',
-                height: 40, // Ajuste la hauteur
-                width: 190,
-                fit: BoxFit.contain, // Garde les proportions
-              ),
-              const SizedBox(width: 10), // Espace entre l'image et le texte,
-            ],
-          ),
-          centerTitle: true,
-        ),
-      body: widget.child,
-      bottomNavigationBar: Container(
-        decoration: const BoxDecoration(
-          color: const Color.fromRGBO(41, 36, 96, 1),
-          borderRadius: const BorderRadius.only(
-            topLeft: const Radius.circular(20),
-            topRight: const Radius.circular(20),
-          )
-        ),
-        child: BottomNavigationBar(
-          onTap: changeTab,
-          backgroundColor: Colors.transparent,
-          currentIndex: currentIndex,
-          unselectedItemColor: Colors.white,
-          selectedItemColor: const Color.fromRGBO(252, 179, 48, 1),
-          items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: const EdgeInsets.only(top: 5),
-                  child: Icon(Icons.home),
-                ),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.book),
-                label: 'Modules',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.verified),
-                label: 'Certification',
-              ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-/*
-class App extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<App> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _pages = [
-    Center(child: Text('Page Home')),
-    Center(child: Text('Page Modules')),
-    Center(child: Text('Page Certification')),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 3700), () {
+      setState(() {
+        showLaunchScreen = false;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'lib/data/AppData/facto-logo.png',
-                height: 40, // Ajuste la hauteur
-                fit: BoxFit.contain, // Garde les proportions
-              ),
-              const SizedBox(width: 10), // Espace entre l'image et le texte
-              const Text('Factoscope'),
-            ],
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color.fromRGBO(252, 179, 48, 1),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'lib/data/AppData/CharteFactoscope/logo-factoscope_seul.png',
+                  height: 40,
+                  width: 190,
+                  fit: BoxFit.contain,
+                ),
+                const SizedBox(width: 10),
+              ],
+            ),
+            centerTitle: true,
           ),
-          centerTitle: true,
+          body: widget.child!,
+          bottomNavigationBar: Container(
+            decoration: const BoxDecoration(
+              color: Color.fromRGBO(41, 36, 96, 1),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+            child: BottomNavigationBar(
+              onTap: changeTab,
+              backgroundColor: Colors.transparent,
+              currentIndex: currentIndex,
+              unselectedItemColor: Colors.white,
+              selectedItemColor: const Color.fromRGBO(252, 179, 48, 1),
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Padding(
+                    padding: EdgeInsets.only(top: 5),
+                    child: Icon(Icons.home),
+                  ),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.book),
+                  label: 'Modules',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.verified),
+                  label: 'Certification',
+                ),
+              ],
+            ),
+          ),
         ),
-        body: _pages[_selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.book),
-              label: 'Modules',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.verified),
-              label: 'Certification',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: const Color.fromRGBO(252, 179, 48, 1),
-          onTap: _onItemTapped,
-        ),
-      ),
+
+        // Met le LaunchScreenView au-dessus de tout
+        if (showLaunchScreen)
+          Positioned.fill(
+            child: LaunchScreenView(),
+          ),
+      ],
     );
   }
 }
-
-
-*/
